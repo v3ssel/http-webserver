a086b93ba497fa7406643f6a061d58eaafeae645
v3ssel <v3ssels@pm.me>
2024-04-15 03:42:36 +0500

    epoll version

--- server_src/server.cpp
+++ server_src/server.cpp
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 
 #include "server.h"
+#include <iostream>
 
 namespace srv {
 SocketServer::SocketServer(const std::string &address, uint16_t port, BaseConnectionLogger* connection_logger)
@@ -30,6 +31,10 @@ void SocketServer::Start() {
 
     is_running_ = true;
     listener_ = std::thread(&SocketServer::Listen, this);
+    CreateEpoll();
+    for (int i = 0; i < kMaxWorkers; i++) {
+        workers_[i] = std::thread(&SocketServer::ProcessEvents, this, i);
+    }
 }
 
 void SocketServer::Close() {
@@ -37,6 +42,11 @@ void SocketServer::Close() {
 
     is_running_ = false;
     listener_.join();
+    for (int i = 0; i < kMaxWorkers; i++) {
+        workers_[i].join();
+        close(worker_fd_[i]);
+    }
+    close(server_fd_);
 }
 
 void SocketServer::CreateSocket() {
@@ -68,10 +78,19 @@ void SocketServer::StartListening() {
     }
 }
 
+void SocketServer::CreateEpoll() {
+    for (int i = 0; i < kMaxWorkers; i++) {
+        if ((worker_fd_[i] = epoll_create1(0)) < 0) {
+            throw std::runtime_error("Epoll cannot be created. " + std::string(strerror(errno)));
+        }
+    }
+}
+
 void SocketServer::Listen() {
     sockaddr_in client_addr;
     socklen_t client_addrlen = sizeof(client_addr);
     int client_fd;
+    int current_worker = 0;
 
     while (is_running_) {
         client_fd = accept(server_fd_, (struct sockaddr*)&client_addr, (socklen_t*)&client_addrlen);
@@ -80,18 +99,94 @@ void SocketServer::Listen() {
             continue;
         }
 
-        char buffer[kBufferSize] = {0};
-        int valread = read(client_fd, buffer, kBufferSize);
+        epoll_event event;
+        event.events = EPOLLIN;
+        event.data.ptr = new int(client_fd);
 
-        if (connection_logger_) {
-            try {
-                connection_logger_->LogConnected(buffer);
-            } catch (...) {
-                continue;
-            }
+        if (epoll_ctl(worker_fd_[current_worker], EPOLL_CTL_ADD, client_fd, &event) != 0) {
+            throw std::runtime_error("Cannot create epoll. " + std::string(strerror(errno)));
+        }
+        current_worker++;
+        if (current_worker == kMaxWorkers) current_worker = 0;
+
+        // char buffer[kBufferSize] = {0};
+        // int valread = read(client_fd, buffer, kBufferSize);
+
+        // if (connection_logger_) {
+        //     try {
+        //         connection_logger_->LogConnected(buffer);
+        //     } catch (...) {
+        //         continue;
+        //     }
+        // }
+
+        // close(client_fd);
+    }
+}
+
+void SocketServer::ProcessEvents(int current_worker) {
+    int worker_fd = worker_fd_[current_worker];
+
+    while (is_running_) {
+        int events_num = epoll_wait(worker_fd, worker_events[current_worker], kMaxEvents, 0);
+
+        if (events_num <= 0) {
+            continue;
         }
 
-        close(client_fd);
+        std::cout << "GOT EVENTS: " << events_num << "\n";
+
+        for (int i = 0; i < events_num; i++) {
+            epoll_event curr_event = worker_events[current_worker][i];
+            int* client_fd = reinterpret_cast<int*>(curr_event.data.ptr);
+
+            if (curr_event.events & EPOLLIN) {
+                HandleEpollInEvent(worker_fd, *client_fd);
+                close(*client_fd);
+                delete client_fd;
+            }
+            // else if (curr_event.events & EPOLLOUT) {
+            //     HandleEpollOutEvent(worker_fd, *client_fd);
+            //     close(*client_fd);
+            //     delete client_fd;
+            // } 
+            else {
+                epoll_ctl(worker_fd_[current_worker], EPOLL_CTL_DEL, *client_fd, nullptr);
+                close(*client_fd);
+                delete client_fd;
+            }
+        }
+    }
+}
+
+void SocketServer::HandleEpollInEvent(int epoll_fd, int client_fd) {
+    char buffer[kBufferSize] = {0};
+    ssize_t bytes_recv = recv(client_fd, buffer, kBufferSize, 0);
+    std::cout << "---------------GOT-------------\n" << buffer << "\n";
+    
+    if (connection_logger_) {
+        connection_logger_->LogConnected(buffer);
+    }
+    // epoll_event event;
+    // event.events = EPOLLOUT;
+    // event.data.ptr = new int(client_fd);
+
+    // if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_fd, &event) != 0) {
+    //     throw std::runtime_error("Something wrong Epoll Ctl in EpollIn. " + std::string(strerror(errno)));
+    // }
+}
+
+void SocketServer::HandleEpollOutEvent(int epoll_fd, int client_fd) {
+    std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello bpb!\n\n";
+    ssize_t bytes_sent = send(client_fd, response.c_str(), response.length(), 0);
+    std::cout << "---------------SENT-------------\n";
+
+    epoll_event event;
+    event.events = EPOLLIN;
+    event.data.ptr = new int(client_fd);
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_MOD, client_fd, &event) != 0) {
+        throw std::runtime_error("Something wrong Epoll Ctl in EpollOut. " + std::string(strerror(errno)));
     }
 }
 }
--- server_src/server.h
+++ server_src/server.h
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <thread>
+
+#include <sys/epoll.h>
+
 #include "base_connection_logger.h"
 
 namespace srv {
@@ -10,6 +13,8 @@ namespace srv {
        public:
         static constexpr int kBacklogSize = 10;
         static constexpr int kBufferSize = 2048;
+        static constexpr int kMaxWorkers = 10;
+        static constexpr int kMaxEvents = 10000;
 
         SocketServer(const std::string& address = "127.0.0.1", uint16_t port = 8088, BaseConnectionLogger* connection_logger = nullptr);
         ~SocketServer();
@@ -21,8 +26,13 @@ namespace srv {
         void CreateSocket();
         void BindSocket();
         void StartListening();
+        void CreateEpoll();
 
         void Listen();
+        void ProcessEvents(int current_worker);
+
+        void HandleEpollInEvent(int epoll_fd, int client_fd);
+        void HandleEpollOutEvent(int epoll_fd, int client_fd);
 
         std::string address_;
         uint16_t port_;
@@ -32,6 +42,10 @@ namespace srv {
         bool is_running_;
 
         std::thread listener_;
+
+        int worker_fd_[kMaxWorkers];
+        std::thread workers_[kMaxWorkers];
+        epoll_event worker_events[kMaxWorkers][kMaxEvents];
     };
 }
 
